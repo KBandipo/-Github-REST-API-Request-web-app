6ba922e3b372d2254bd9e9f27eb4dfbcfcc84c15
KBandipo~ <keneebandipo@gmail.com>
2024-05-15 10:39:06 +0100

    Add function to fetch GitHub issues data

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const getButton = document.getElementById("getButton");
 const postButton = document.getElementById("postButton");
 const resultDiv = document.getElementById("result");
+
+// GET request Function
+function makeGetRequest() {
+  // Make a GET request to fetch data related to issues or pull requests
+  fetch(
+    "https://api.github.com/repos/KBandipo/Alstride-E-commerce-Website/issues"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      resultDiv.innerHTML = "";
+
+      data.forEach((issue) => {
+        const issueLink = document.createElement("a");
+        issueLink.href = issue.html_url;
+        issueLink.textContent = issue.title;
+        issueLink.target = "_blank";
+        resultDiv.appendChild(issueLink);
+        resultDiv.appendChild(document.createElement("br"));
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+}
